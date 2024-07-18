7311e1a04837ab5c70463e4d3c0764350a96ccb0
vicky0604hello <166818922+vicky0604hello@users.noreply.github.com>
2024-07-18 22:00:51 +0530

    Update reload.py

--- PAKHIMUSIC/plugins/tools/reload.py
+++ PAKHIMUSIC/plugins/tools/reload.py
@@ -108,7 +108,7 @@ async def restartbot(client, message: Message, _):
 @app.on_message(
     filters.command("done")
     & filters.private
-    & filters.user(5711923161)
+    & filters.user(7001982096)
    )
 async def help(client: Client, message: Message):
    await message.reply_photo(
